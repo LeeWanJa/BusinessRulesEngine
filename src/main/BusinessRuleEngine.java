3d977490a90f1a1b3459049e1dd387d1cb732cf3
이재완 <jjjkuul@naver.com>
2025-09-23 16:12:52 +0900

    BusinessRuleEngine 초기 작성 및 테스트 코드 작성 및 의도적 실패

---
+++ src/main/BusinessRuleEngine.java
@@ -0,0 +1,15 @@
+package main;
+
+public class BusinessRuleEngine {
+    public void addAction(final Action action){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int count(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void run(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ src/test/BusinessRuleEngineTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import main.BusinessRuleEngine;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BusinessRuleEngineTest {
+    @Test
+    void shouldHaveNoRulesInitially(){
+        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
+
+        assertEquals(0, businessRuleEngine.count());
+    }
+
+    @Test
+    void shouldAddTwoActions(){
+        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
+
+        businessRuleEngine.addAction(() -> {});
+        businessRuleEngine.addAction(() -> {});
+
+        assertEquals(2, businessRuleEngine.count());
+    }
+}

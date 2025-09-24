ba1d42523935a85e1c4e1c5acfc93f044ac55acb
이재완 <jjjkuul@naver.com>
2025-09-24 22:20:56 +0900

    enum Stage을 만든 뒤 switch문과 if-else문 비교

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,9 +4,48 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        Facts facts = new Facts();
-        facts.addFact("Mark", "CEO");
+        Facts fact = new Facts();
+        fact.addFact("jobTitle", "CEO");
 
-        BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine(facts);
+        BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine(fact);
+        businessRuleEngine.addAction(facts -> {
+            var jobTitle = facts.getFact("jobTitle");
+            if("CEO".equals(jobTitle)) {
+                var name = facts.getFact("name");
+                // Mailer.sendEmail("sales@company.com", "Relevant customer: " + name);
+                // 아직 미구현
+            }
+        });
+        /*
+            // 컴파일러가 속으로 생각하며 만드는 코드 (실제로는 더 최적화됨)
+            new Action() {
+                @Override
+                public void perform(Facts facts) {
+                    // 람다식의 { ... } 안의 내용이 그대로 들어옴
+                    var jobTitle = facts.getFact("jobTitle");
+                    if("CEO".equals(jobTitle)) {
+                        var name = facts.getFact("name");
+                        Mailer.sendEmail("sales@company.com", "Relevant customer: " + name);
+                    }
+                }
+            }
+         */
+
+        // enum, switch 사용
+        businessRuleEngine.addAction(facts -> {
+            var forecastedAmount = 0.0;
+            var dealStage = Stage.valueOf(facts.getFact("stage"));
+            var amount = Double.parseDouble(facts.getFact("amount"));
+            if(dealStage == Stage.LEAD){
+                forecastedAmount = amount * 0.2;
+            } else if(dealStage == Stage.INTERESTED){
+                forecastedAmount = amount * 0.8;
+            } else if(dealStage == Stage.EVALUATING){
+                forecastedAmount = amount * 0.5;
+            } else if(dealStage == Stage.CLOSED){
+                forecastedAmount = amount;
+            }
+            facts.addFact("forecastedAmount", String.valueOf(forecastedAmount));
+        });
     }
 }
---
+++ src/main/Stage.java
@@ -0,0 +1,5 @@
+package main;
+
+public enum Stage{
+    LEAD, INTERESTED, EVALUATING, CLOSED
+}

00cd24daeb9199b5603e22e0035d97d96be9118f
이재완 <jjjkuul@naver.com>
2025-09-24 22:34:36 +0900

    강력한 업그레이드 switch

--- src/main/Main.java
+++ src/main/Main.java
@@ -36,15 +36,32 @@ public class Main {
             var forecastedAmount = 0.0;
             var dealStage = Stage.valueOf(facts.getFact("stage"));
             var amount = Double.parseDouble(facts.getFact("amount"));
-            if(dealStage == Stage.LEAD){
-                forecastedAmount = amount * 0.2;
-            } else if(dealStage == Stage.INTERESTED){
-                forecastedAmount = amount * 0.8;
-            } else if(dealStage == Stage.EVALUATING){
-                forecastedAmount = amount * 0.5;
-            } else if(dealStage == Stage.CLOSED){
-                forecastedAmount = amount;
+
+            /*
+            switch(dealStage){
+                case LEAD:
+                    forecastedAmount = amount * 0.2;
+                    break;
+                case EVALUATING:
+                    forecastedAmount = amount * 0.5;
+                    break;
+                case INTERESTED:
+                    forecastedAmount = amount * 0.8;
+                    break;
+                case CLOSED:
+                    forecastedAmount = amount;
+                    break;
             }
+             */
+
+            // 새로운 switch문 형태 -> 모든 가능성을 확인하는 소모 검사도 이루어짐
+            forecastedAmount = switch (dealStage) {
+                case LEAD -> amount * 0.2;
+                case EVALUATING -> amount * 0.5;
+                case INTERESTED -> amount * 0.8;
+                case CLOSED -> amount;
+            };
+
             facts.addFact("forecastedAmount", String.valueOf(forecastedAmount));
         });
     }
